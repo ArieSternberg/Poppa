76d5d1340f4f96cd16085f127482122f23b434f0
Arie Sternberg <sternbergarie@gmail.com>
2025-01-13 12:51:24 -0500

    params error fix

--- src/app/dashboard/elder/[id]/page.tsx
+++ src/app/dashboard/elder/[id]/page.tsx
@@ -1,9 +1,13 @@
 import { ElderDashboardComponent } from '@/components/elder-dashboard'
 
-export default function Page({
-  params,
-}: {
-  params: { id: string };
-}) {
-  return <ElderDashboardComponent elderId={params.id} />
+interface PageProps {
+  params: Promise<{
+    id: string;
+  }>;
+  searchParams: { [key: string]: string | string[] | undefined };
+}
+
+export default async function Page({ params, searchParams }: PageProps) {
+  const { id } = await params;
+  return <ElderDashboardComponent elderId={id} />
 } 
